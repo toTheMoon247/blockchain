e324466ab567a0e0b703b872de03e7d9c4a1697a
Sam <sam.haim@paconsulting.com>
2021-12-12 21:50:53 +0000

    wip: transactions-pool

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -22,6 +26,17 @@ app.post('/mine', (req, res) => {
 	p2pServer.syncChains();
 
 	res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+	res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+	const recipient = req.body.recipient
+	const amount = req.body.amount;
+	const transaction = wallet.createTransaction(recipient, amount, tp);
+	res.redirect('/transactions');
 });
 
 app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}...`));
